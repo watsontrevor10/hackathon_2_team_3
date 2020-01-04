1d112098fac2bcc787330bbdde1cde9c9ddc3eba
Trevor Watson <watsontrevor10@gmail.com>
2020-01-04 11:26:11 -0700

    added CRUD logic to video controller and created routes

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Header, } from 'semantic-ui-react';
 
-const Home = () => (
-  <Header as="h3" textAlign="center">Devise Auth App</Header>
-);
+const Home = () => {
+  const [ video, setVideo ] = useState([])
+
+  useEffect( () => {
+    axios.get('/api/videos')
+      .then( res => {
+        setVideo(res.data)
+      })
+  }, [])
+
+  
+
+  return (
+    <Header as="h3" textAlign="center">Videos</Header>
+    
+  )
+}
 
 export default Home;
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
-    get 'comments/index'
+   resources :videos
   end
 
   namespace :api do
-    get 'comments/show'
-  end
-
-  namespace :api do
-    get 'comments/create'
-  end
-
-  namespace :api do
-    get 'comments/update'
-  end
-
-  namespace :api do
-    get 'comments/destroy'
+    resources :comments
   end
 
   mount_devise_token_auth_for 'User', at: 'api/auth'
