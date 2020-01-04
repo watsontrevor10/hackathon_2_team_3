60e969f51076a3d4fc2bebd20711932a0b9d5a9c
Trevor Watson <watsontrevor10@gmail.com>
2020-01-04 13:07:07 -0700

    added faker seed file, and logos to assets folder

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
 10.times do
 Video.create(
   title: Faker::Hacker.adjective,
-  duration: 13.52,
-  genre: "Comedy",
+  duration: Faker::Number.decimal(l_digits: 2),
+  genre: Faker::Game.genre,
   description: Faker::Hacker.say_something_smart,
   trailer: "https://youtu.be/XGrYq7OWgdM",
   user_id: 1
 )
 end
+puts "added 10 videos"
