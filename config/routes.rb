d4945fdf35c1fd48704acfc2c39e6875fa40abe7
Trevor Watson <watsontrevor10@gmail.com>
2020-01-04 10:23:05 -0700

    added comments model and video/comment controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'comments/index'
+  end
+
+  namespace :api do
+    get 'comments/show'
+  end
+
+  namespace :api do
+    get 'comments/create'
+  end
+
+  namespace :api do
+    get 'comments/update'
+  end
+
+  namespace :api do
+    get 'comments/destroy'
+  end
+
   mount_devise_token_auth_for 'User', at: 'api/auth'
 end
